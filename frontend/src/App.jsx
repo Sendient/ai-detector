a5a71d8f8d51d9bfd2fad296c49bf0d4d802f51c
Mohammed ALi <mali@sendient.ai>
2025-05-30 16:19:02 +0100

    frontend deploy issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ import SubscriptionsPage from './pages/SubscriptionsPage';
 import ClassViewPage from './pages/ClassViewPage';
 import AdminPage from './pages/AdminPage';
 import AdminStudentsPage from './pages/AdminStudentsPage';
-import AdminManageClassesPage from '@/pages/admin/AdminManageClassesPage';
+import AdminManageClassesPage from '@/pages/admin/AdminManageClassesPage.jsx';
 
 function App() {
   const { isAuthenticated, isLoading, login } = useKindeAuth();
